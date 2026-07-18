780bdae70b6723852185f93340a0f7e66d130b0b
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-18 15:56:47 +0100

    all done

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import { Box } from '@mui/material';
 import { useFundSummary } from './hooks/useFundSummary';
 import { FundSummaryTable } from './components/FundSummaryTable';
 
+const TODAY_TIMESTAMP = '2026-07-11T14:54:09.780Z';
+
 const VANECK_PRICES_SHEET = 'vaneck_NL0011683594';
 const VANECK_DIVIDENDS_SHEET = 'vaneck_NL0011683594_dividends';
 const GLOBAL_SELECT_PRICES_SHEET = 'GlobalSelect_DE000A0F5UH1';
 const GLOBAL_SELECT_DIVIDENDS_SHEET = 'GlobalSelect_DE000A0F5UH1_dividends';
 
 function App() {
-    const sevenDaysAgo = useMemo(() => dayjs().subtract(7, 'day'), []);
+    const sevenDaysAgo = useMemo(() => dayjs(TODAY_TIMESTAMP), []);
     const fiveYearsAgo = useMemo(() => sevenDaysAgo.subtract(5, 'year'), [sevenDaysAgo]);
 
     const vaneck = useFundSummary(VANECK_PRICES_SHEET, VANECK_DIVIDENDS_SHEET, fiveYearsAgo, sevenDaysAgo);
