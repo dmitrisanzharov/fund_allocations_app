e1d09b9a2b054627936b326706adec4fa1a0d5a3
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-18 15:52:16 +0100

    now we have table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import dayjs from 'dayjs';
 import { useMemo } from 'react';
-import { Box, Divider } from '@mui/material';
+import { Box } from '@mui/material';
 import { useFundSummary } from './hooks/useFundSummary';
+import { FundSummaryTable } from './components/FundSummaryTable';
 
 const VANECK_PRICES_SHEET = 'vaneck_NL0011683594';
 const VANECK_DIVIDENDS_SHEET = 'vaneck_NL0011683594_dividends';
@@ -20,25 +21,17 @@ function App() {
         sevenDaysAgo
     );
 
+    const funds = useMemo(
+        () => [
+            { name: 'VanEck', ...vaneck },
+            { name: 'GlobalSelect', ...globalSelect },
+        ],
+        [vaneck, globalSelect]
+    );
+
     return (
         <Box sx={{ p: 4 }}>
-            <Box>
-                <Box sx={{ textDecoration: 'underline', mb: 1 }}>VanEck</Box>
-                <Box>oldest: {vaneck.oldest?.Price}</Box>
-                <Box>newest: {vaneck.newest?.Price}</Box>
-                <Box sx={{ mb: 1 }}>total dividends: {vaneck.totalDividends.toFixed(2)}</Box>
-                <Box>correct % change including dividends: {vaneck.correctDifferenceAsPercent}</Box>
-                <Box>average dividend yield for this period: {vaneck.averageDividendYield}%</Box>
-            </Box>
-            <Divider sx={{ my: 4 }} />
-            <Box>
-                <Box sx={{ textDecoration: 'underline', mb: 1 }}>GlobalSelect</Box>
-                <Box>oldest: {globalSelect.oldest?.Price}</Box>
-                <Box>newest: {globalSelect.newest?.Price}</Box>
-                <Box sx={{ mb: 1 }}>total dividends: {globalSelect.totalDividends.toFixed(2)}</Box>
-                <Box>correct % change including dividends: {globalSelect.correctDifferenceAsPercent}</Box>
-                <Box>average dividend yield for this period: {globalSelect.averageDividendYield}%</Box>
-            </Box>
+            <FundSummaryTable funds={funds} />
         </Box>
     );
 }
