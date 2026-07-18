7aa7eb696a3854750bb77fa9442b6c3e5af0980b
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-18 15:07:44 +0100

    dates

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+import { useMemo } from 'react';
 import { useSheetData } from './hooks/useSheetData';
 import { SheetTable } from './components/SheetTable';
 import { DividendRow, PriceRow } from './services/googleSheets';
@@ -8,6 +10,8 @@ const GLOBAL_SELECT_PRICES_SHEET = 'GlobalSelect_DE000A0F5UH1';
 const GLOBAL_SELECT_DIVIDENDS_SHEET = 'GlobalSelect_DE000A0F5UH1_dividends';
 
 function App() {
+    const sevenDaysAgo = useMemo(() => dayjs().subtract(7, 'day'), []);
+    const fiveYearsAgo = useMemo(() => sevenDaysAgo.subtract(5, 'year'), [sevenDaysAgo]);
 
     console.log('============================');
     const vaneck_prices = useSheetData<PriceRow>(VANECK_PRICES_SHEET);
@@ -15,11 +19,11 @@ function App() {
     const vaneck_dividends = useSheetData<DividendRow>(VANECK_DIVIDENDS_SHEET);
     console.log("vaneck_dividends: ", vaneck_dividends.rows);
 
-    console.log('============================');
-    const global_select_prices = useSheetData<PriceRow>(GLOBAL_SELECT_PRICES_SHEET);
-    console.log("global_select_prices: ", global_select_prices.rows);
-    const global_select_dividends = useSheetData<DividendRow>(GLOBAL_SELECT_DIVIDENDS_SHEET);
-    console.log("global_select_dividends: ", global_select_dividends.rows);
+    // console.log('============================');
+    // const global_select_prices = useSheetData<PriceRow>(GLOBAL_SELECT_PRICES_SHEET);
+    // console.log("global_select_prices: ", global_select_prices.rows);
+    // const global_select_dividends = useSheetData<DividendRow>(GLOBAL_SELECT_DIVIDENDS_SHEET);
+    // console.log("global_select_dividends: ", global_select_dividends.rows);
 
     return (
         <>
