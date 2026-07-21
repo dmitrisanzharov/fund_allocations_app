478a6ca12c0ba5b718b1e63729a0655129166e18
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-21 14:39:01 +0100

    all working

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,16 @@ import { Box, Divider } from '@mui/material';
 import { useFundSummary } from './hooks/useFundSummary';
 import { FundSummaryTable } from './components/FundSummaryTable';
 import { parseFundSheetName } from './utils/fundIdentifier';
+import { FUNDS, FundConfig, TODAY_TIMESTAMP } from './constants';
 
-export const TODAY_TIMESTAMP = '2026-07-15T18:00:00.780Z';
+function getFundConfig(id: (typeof FUNDS)[number]['id']): FundConfig {
+    const config = FUNDS.find((fund) => fund.id === id);
+    if (!config) {
+        throw new Error(`Missing fund config for id: ${id}`);
+    }
 
-export const VANECK_PRICES_SHEET = 'vaneck_NL0011683594';
-export const VANECK_DIVIDENDS_SHEET = 'vaneck_NL0011683594_div';
-export const GLOBAL_SELECT_PRICES_SHEET = 'GlobalSelect_DE000A0F5UH1';
-export const GLOBAL_SELECT_DIVIDENDS_SHEET = 'GlobalSelect_DE000A0F5UH1_div';
-export const VANGUARD_PRICES_SHEET = 'VanGuard_IE00B8GKDB10';
-export const VANGUARD_DIVIDENDS_SHEET = 'VanGuard_IE00B8GKDB10_div';
-export const INVESCO_EU_PRICES_SHEET = 'InvescoEu_IE00BZ4BMM98';
-export const INVESCO_EU_DIVIDENDS_SHEET = 'InvescoEu_IE00BZ4BMM98_div';
-export const ISHARE_EU_SELECT_PRICES_SHEET = 'iShareEuSelect_DE0002635281';
-export const ISHARE_EU_SELECT_DIVIDENDS_SHEET = 'iShareEuSelect_DE0002635281_div';
-export const ISHARE_EU_BANK_PRICES_SHEET = 'iSharesEuBank_DE000A0F5UJ7';
-export const ISHARE_EU_BANK_DIVIDENDS_SHEET = 'iSharesEuBank_DE000A0F5UJ7_div';
-export const ISHARE_UK_PRICES_SHEET = 'iSharesUK_IE00B0M63060';
-export const ISHARE_UK_DIVIDENDS_SHEET = 'iSharesUK_IE00B0M63060_div';
+    return config;
+}
 
 function App() {
     const lastWeekWednesday = useMemo(() => dayjs(TODAY_TIMESTAMP), []);
@@ -30,75 +23,88 @@ function App() {
     );
 
     // funds
-    const vaneckIdentifier = parseFundSheetName(VANECK_PRICES_SHEET);
+    const vaneckConfig = getFundConfig('vaneck');
+    const vaneckIdentifier = parseFundSheetName(vaneckConfig.pricesSheet);
     const vaneck = useFundSummary(
         vaneckIdentifier.name,
         vaneckIdentifier.isin,
-        VANECK_PRICES_SHEET,
-        VANECK_DIVIDENDS_SHEET,
+        vaneckConfig.pricesSheet,
+        vaneckConfig.dividendsSheet,
         fiveYearsAgo,
         lastWeekWednesday,
-        0.15
+        vaneckConfig.taxRate
     );
 
-    const globalSelectIdentifier = parseFundSheetName(GLOBAL_SELECT_PRICES_SHEET);
+    const globalSelectConfig = getFundConfig('globalSelect');
+    const globalSelectIdentifier = parseFundSheetName(globalSelectConfig.pricesSheet);
     const globalSelect = useFundSummary(
         globalSelectIdentifier.name,
         globalSelectIdentifier.isin,
-        GLOBAL_SELECT_PRICES_SHEET,
-        GLOBAL_SELECT_DIVIDENDS_SHEET,
+        globalSelectConfig.pricesSheet,
+        globalSelectConfig.dividendsSheet,
         fiveYearsAgo,
-        lastWeekWednesday
+        lastWeekWednesday,
+        globalSelectConfig.taxRate
     );
 
-    const vanguardIdentifier = parseFundSheetName(VANGUARD_PRICES_SHEET);
+    const vanguardConfig = getFundConfig('vanguard');
+    const vanguardIdentifier = parseFundSheetName(vanguardConfig.pricesSheet);
     const vanguard = useFundSummary(
         vanguardIdentifier.name,
         vanguardIdentifier.isin,
-        VANGUARD_PRICES_SHEET,
-        VANGUARD_DIVIDENDS_SHEET,
+        vanguardConfig.pricesSheet,
+        vanguardConfig.dividendsSheet,
         fiveYearsAgo,
-        lastWeekWednesday
+        lastWeekWednesday,
+        vanguardConfig.taxRate
     );
 
-    const invescoEuIdentifier = parseFundSheetName(INVESCO_EU_PRICES_SHEET);
+    const invescoEuConfig = getFundConfig('invescoEu');
+    const invescoEuIdentifier = parseFundSheetName(invescoEuConfig.pricesSheet);
     const invescoEu = useFundSummary(
         invescoEuIdentifier.name,
         invescoEuIdentifier.isin,
-        INVESCO_EU_PRICES_SHEET,
-        INVESCO_EU_DIVIDENDS_SHEET,
+        invescoEuConfig.pricesSheet,
+        invescoEuConfig.dividendsSheet,
         fiveYearsAgo,
-        lastWeekWednesday
+        lastWeekWednesday,
+        invescoEuConfig.taxRate
     );
 
-    const ishareEuSelectIdentifier = parseFundSheetName(ISHARE_EU_SELECT_PRICES_SHEET);
+    const ishareEuSelectConfig = getFundConfig('ishareEuSelect');
+    const ishareEuSelectIdentifier = parseFundSheetName(ishareEuSelectConfig.pricesSheet);
     const ishareEuSelect = useFundSummary(
         ishareEuSelectIdentifier.name,
         ishareEuSelectIdentifier.isin,
-        ISHARE_EU_SELECT_PRICES_SHEET,
-        ISHARE_EU_SELECT_DIVIDENDS_SHEET,
+        ishareEuSelectConfig.pricesSheet,
+        ishareEuSelectConfig.dividendsSheet,
         fiveYearsAgo,
-        lastWeekWednesday
+        lastWeekWednesday,
+        ishareEuSelectConfig.taxRate
     );
 
-    const ishareEuBankIdentifier = parseFundSheetName(ISHARE_EU_BANK_PRICES_SHEET);
+    const ishareEuBankConfig = getFundConfig('ishareEuBank');
+    const ishareEuBankIdentifier = parseFundSheetName(ishareEuBankConfig.pricesSheet);
     const ishareEuBank = useFundSummary(
         ishareEuBankIdentifier.name,
         ishareEuBankIdentifier.isin,
-        ISHARE_EU_BANK_PRICES_SHEET,
-        ISHARE_EU_BANK_DIVIDENDS_SHEET,
+        ishareEuBankConfig.pricesSheet,
+        ishareEuBankConfig.dividendsSheet,
         fiveYearsAgo,
-        lastWeekWednesday
+        lastWeekWednesday,
+        ishareEuBankConfig.taxRate
     );
 
-    const ishareUkIdentifier = parseFundSheetName(ISHARE_UK_PRICES_SHEET);
+    const ishareUkConfig = getFundConfig('ishareUk');
+    const ishareUkIdentifier = parseFundSheetName(ishareUkConfig.pricesSheet);
     const ishareUk = useFundSummary(
         ishareUkIdentifier.name,
         ishareUkIdentifier.isin,
-        ISHARE_UK_PRICES_SHEET,
-        ISHARE_UK_DIVIDENDS_SHEET,
+        ishareUkConfig.pricesSheet,
+        ishareUkConfig.dividendsSheet,
         fiveYearsAgo,
-        lastWeekWednesday
+        lastWeekWednesday,
+        ishareUkConfig.taxRate
     );
 
     const funds = useMemo(
