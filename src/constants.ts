42b34f8de08dd4628354d9c3f00653c2babdf628
VictorSarov <victor.sarov@rentalmatics.com>
2026-07-21 14:29:34 +0100

    const added

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+export const FUND_TIER_OBJ = {
+    tier_1: {
+        shortName: 'T1',
+        longName: 'TIER 1: Global Core ... world / LARGE impact on the world',
+        maxAllocation: 0.7
+    },
+    EU: {
+        shortName: 'EU',
+        longName: 'euro region',
+        maxAllocation: 0.2
+    },
+    tier_2: {
+        shortName: 'T2',
+        longName: 'Tier 2: Regional, but NOT high impact on the world',
+        maxAllocation: 0.1
+    }
+};
+
